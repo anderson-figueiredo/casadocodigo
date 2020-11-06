685d357698b2604008a4808e33d203c2486d79f6
Caio Bugorin <caio.bugorin@caelum.com.br>
2020-11-06 15:34:59 -0300

    deixando o codigo mais defencivo

--- src/main/java/br/com/dev/casadocodigo/Autor.java
+++ src/main/java/br/com/dev/casadocodigo/Autor.java
@@ -1,9 +1,9 @@
 package br.com.dev.casadocodigo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.springframework.util.Assert;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @Entity
 public class Autor {
@@ -11,15 +11,24 @@ public class Autor {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String nome;
 
+    @NotBlank
+    @Email
     private String email;
 
+    @Max(value = 400)
+    @NotBlank
     private String descricao;
 
+    @Deprecated
     public Autor(){}
 
     public Autor(String nome, String email, String descricao){
+        Assert.hasText(nome, "Nome do autor deve ser preenchido");
+        Assert.hasText(email, "Email do autor deve ser preenchido");
+        Assert.hasText(descricao, "Descrição do autor deve ser preenchido");
         this.nome = nome;
         this.email = email;
         this.descricao = descricao;
