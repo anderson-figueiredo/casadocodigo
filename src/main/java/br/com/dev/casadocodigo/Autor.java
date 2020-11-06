08233d57f8361acf8d5b719a3a8e073a7b843c2a
Jonilson <jonilsonds9@gmail.com>
2020-11-06 14:50:03 -0300

    iniciando o card de autor

---
+++ src/main/java/br/com/dev/casadocodigo/Autor.java
@@ -0,0 +1,11 @@
+package br.com.dev.casadocodigo;
+
+public class Autor {
+
+    private String nome;
+    private String email;
+    private String descricao;
+
+
+
+}
---
+++ src/main/java/br/com/dev/casadocodigo/AutorController.java
@@ -0,0 +1,14 @@
+package br.com.dev.casadocodigo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AutorController {
+
+    @PostMapping
+    public String create() {
+
+    }
+
+}
