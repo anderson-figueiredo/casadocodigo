a307c910708df12346ff56792554335cbb052249
thaisandre <thaisandre.sc@gmail.com>
2020-11-06 14:59:42 -0300

    [wip] iniciando cadastro de autor

--- src/main/java/br/com/dev/casadocodigo/AutorController.java
+++ src/main/java/br/com/dev/casadocodigo/AutorController.java
@@ -1,13 +1,17 @@
 package br.com.dev.casadocodigo;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
 
 @Controller
 public class AutorController {
 
     @PostMapping
-    public String create() {
+    public ResponseEntity<?> create(@RequestBody @Valid AutorForm autorForm) {
 
     }
 
---
+++ src/main/java/br/com/dev/casadocodigo/AutorForm.java
@@ -0,0 +1,36 @@
+package br.com.dev.casadocodigo;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+
+public class AutorForm {
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @Max(value=400)
+    @NotBlank
+    private String descricao;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Autor toEntity() {
+        return new Autor(this.nome);
+    }
+}
