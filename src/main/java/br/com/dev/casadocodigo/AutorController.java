de308debe9bb3cb2de03def784feac303fdcfbc3
Wanderson M <wandersonmaceds@gmail.com>
2020-11-06 15:21:40 -0300

    [wip] finalizando cadastro de autor

--- src/main/java/br/com/dev/casadocodigo/AutorController.java
+++ src/main/java/br/com/dev/casadocodigo/AutorController.java
@@ -4,24 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/autor")
 public class AutorController {
 
     @Autowired
     private AutorRepository autorRepository;
 
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody @Valid AutorForm autorForm, BindingResult result) {
+    public ResponseEntity<?> create(@RequestBody @Valid AutorForm autorForm) {
 
-        if(result.hasErrors()){
-            // TODO: :)
-        }
+//        if(result.hasErrors()){
+//            // TODO: :)
+//            return ResponseEntity.badRequest().build();
+//        }
 
         autorRepository.save(autorForm.toEntity());
 
