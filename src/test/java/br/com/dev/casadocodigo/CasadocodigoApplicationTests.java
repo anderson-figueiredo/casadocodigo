fdae9b70de8056c2518894b0991665699f643316
Anderson figueiredo <figueiredo.anderson@rocketmail.com>
2020-11-06 14:32:00 -0300

    Setup do projeto nossa casa do codigo

---
+++ src/test/java/br/com/dev/casadocodigo/CasadocodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadocodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
