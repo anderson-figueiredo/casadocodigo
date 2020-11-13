7dad326d10ddcfa3832b1d10879fe434e339ae3f
Anderson figueiredo <figueiredo.anderson@rocketmail.com>
2020-11-13 16:12:52 -0300

    Testando unicidade do nome da categoria

--- src/test/java/br/com/dev/casadocodigo/categoria/CategoriaControllerTest.java
+++ src/test/java/br/com/dev/casadocodigo/categoria/CategoriaControllerTest.java
@@ -1,13 +1,29 @@
 package br.com.dev.casadocodigo.categoria;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class CategoriaControllerTest {
 
-    @Test
-    public void erro_categoria_unica() {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Test
+    public void erro_nome_unico() throws Exception {
+
+        categoriaRepository.save(new Categoria("existente"));
+        mockMvc.perform(post("/categorias").contentType(MediaType.APPLICATION_JSON).content("{\"nome\":\"existente\"}")).andExpect(status().isBadRequest());
     }
 }
