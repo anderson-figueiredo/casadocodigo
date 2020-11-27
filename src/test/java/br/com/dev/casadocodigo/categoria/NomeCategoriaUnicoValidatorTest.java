ee7f8012cb6deaf0cdf24f99ef109418b7f12bf3
geovaniggr <geovani.hilario@outlook.com>
2020-11-27 15:53:33 -0300

    [WIP] Adicionando teste de validacao de nome de categoria unico

--- src/test/java/br/com/dev/casadocodigo/categoria/NomeCategoriaUnicoValidatorTest.java
+++ src/test/java/br/com/dev/casadocodigo/categoria/NomeCategoriaUnicoValidatorTest.java
@@ -1,9 +1,30 @@
 package br.com.dev.casadocodigo.categoria;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.validation.Errors;
+
+import static org.mockito.Mockito.when;
 
 class NomeCategoriaUnicoValidatorTest {
 
+    private CategoriaRepository categoriaRepository;
+    private Errors errors;
+    private NovaCategoriaRequest novaCategoriaRequest;
+    private NomeCategoriaUnicoValidator nomeCategoriaUnicoValidator;
 
+    @BeforeEach
+    public void setup(){
+        this.categoriaRepository = Mockito.mock(CategoriaRepository.class);
+        this.errors = Mockito.mock(Errors.class);
+        this.novaCategoriaRequest = Mockito.mock(NovaCategoriaRequest.class);
+        this.nomeCategoriaUnicoValidator = new NomeCategoriaUnicoValidator(categoriaRepository);
+    }
+
+    @Test
+    public void validate__deveria_registrar_erro_se_nome_ja_existente(){
+        when();
+    }
 
 }
