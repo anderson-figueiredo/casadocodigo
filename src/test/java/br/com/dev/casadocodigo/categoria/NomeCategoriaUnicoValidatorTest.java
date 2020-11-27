99bc4ddd515c2c2daf69fb674b945941d3cbd032
JoaoHL <jao.luciano8@gmail.com>
2020-11-27 16:02:34 -0300

    Finalizando teste de NomeCategoriaUnicoValidator

--- src/test/java/br/com/dev/casadocodigo/categoria/NomeCategoriaUnicoValidatorTest.java
+++ src/test/java/br/com/dev/casadocodigo/categoria/NomeCategoriaUnicoValidatorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.validation.Errors;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class NomeCategoriaUnicoValidatorTest {
 
@@ -24,7 +24,18 @@ class NomeCategoriaUnicoValidatorTest {
 
     @Test
     public void validate__deveria_registrar_erro_se_nome_ja_existente(){
-        when();
+        when(novaCategoriaRequest.getNome()).thenReturn("Mobile");
+        when(categoriaRepository.existsByNome("Mobile")).thenReturn(true);
+        nomeCategoriaUnicoValidator.validate(novaCategoriaRequest, errors);
+        verify(errors, times(1)).rejectValue("nome", "categoria.nome.unico");
+    }
+
+    @Test
+    public void validate__nao_deveria_registrar_erro_se_nome_nao_existente(){
+        when(novaCategoriaRequest.getNome()).thenReturn("Design");
+        when(categoriaRepository.existsByNome("Design")).thenReturn(false);
+        nomeCategoriaUnicoValidator.validate(novaCategoriaRequest, errors);
+        verify(errors, never()).rejectValue("nome", "categoria.nome.unico");
     }
 
 }
