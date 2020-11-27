6912fa77ade39cefe14d4745cbc19c1e5784ea57
Anderson figueiredo <figueiredo.anderson@rocketmail.com>
2020-11-27 15:33:40 -0300

    [WIP] Continuando NovaCategoriaRequestBuilder

--- src/test/java/br/com/dev/casadocodigo/categoria/NovaCategoriaRequestBuilder.java
+++ src/test/java/br/com/dev/casadocodigo/categoria/NovaCategoriaRequestBuilder.java
@@ -1,9 +1,25 @@
 package br.com.dev.casadocodigo.categoria;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class NovaCategoriaRequestBuilder {
 
     public NovaCategoriaRequest umaNovaCategoriaRequestBuilder;
 
 
-    public static
+    public static NovaCategoriaRequestBuilder umaCategoria(){
+        return new NovaCategoriaRequestBuilder();
+    }
+
+    public nova
+
+    public String criaCategoriaComoJson() {
+        try {
+            return new ObjectMapper().writeValueAsString(this);
+        }catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
