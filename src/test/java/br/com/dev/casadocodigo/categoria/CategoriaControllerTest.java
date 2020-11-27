fb96b88ab1e63084d4ff3fac101f962e3edd015d
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-11-27 14:59:33 -0300

    criando teste de nome de categoria vazio ou nulo

--- src/test/java/br/com/dev/casadocodigo/categoria/CategoriaControllerTest.java
+++ src/test/java/br/com/dev/casadocodigo/categoria/CategoriaControllerTest.java
@@ -1,6 +1,9 @@
 package br.com.dev.casadocodigo.categoria;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,8 +25,18 @@ class CategoriaControllerTest {
 
     @Test
     public void erro_nome_unico() throws Exception {
-
         categoriaRepository.save(new Categoria("existente"));
         mockMvc.perform(post("/categorias").contentType(MediaType.APPLICATION_JSON).content("{\"nome\":\"existente\"}")).andExpect(status().isBadRequest());
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void erro_nome_nulo_ou_vazio(String nome) throws Exception {
+        NovaCategoriaRequest novaCategoriaRequest = new NovaCategoriaRequest();
+        novaCategoriaRequest.setNome(nome);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(novaCategoriaRequest);
+
+        mockMvc.perform(post("/categorias").contentType(MediaType.APPLICATION_JSON).content(json)).andExpect(status().isBadRequest());
     }
 }
