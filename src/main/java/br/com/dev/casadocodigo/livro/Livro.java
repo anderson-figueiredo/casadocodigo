90561cb193b1fe355d12b808e174be8cbc2ee73e
Anderson figueiredo <figueiredo.anderson@rocketmail.com>
2021-01-08 15:23:53 -0300

    Continuando com o trabalho no Livro, adicionando validações

--- src/main/java/br/com/dev/casadocodigo/livro/Livro.java
+++ src/main/java/br/com/dev/casadocodigo/livro/Livro.java
@@ -2,11 +2,11 @@ package br.com.dev.casadocodigo.livro;
 
 import br.com.dev.casadocodigo.autor.Autor;
 import br.com.dev.casadocodigo.categoria.Categoria;
+import org.hibernate.validator.constraints.ISBN;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -16,13 +16,25 @@ import static javax.persistence.GenerationType.*;
 public class Livro {
     @Id @GeneratedValue(strategy = IDENTITY)
     private Long id;
+
+    @NotBlank @Column(unique = true)
     private String titulo;
+
+    @NotBlank @Size(max = 500)
     private String resumo;
     private String sumario;
+
+    @NotNull @DecimalMin("20")
     private BigDecimal preco;
     private int numeroDePaginas;
+
+    @NotBlank @ISBN @Column(unique = true)
     private String isbn;
     private LocalDate dataDePublicacao;
+
+    @NotNull @Valid
     private Categoria categoria;
+
+    @NotNull
     private Autor autor;
 }
